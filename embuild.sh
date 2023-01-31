f2d09431550fd7cfd5a1b1d5bcdd4105042cfda3
a dinosaur <dino@a-dinosaur.com>
2023-02-01 05:23:36 +1100

    update build scripts

--- embuild.sh
+++ embuild.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env bash
-mkdir -p embuild
-cp resources/* embuild/
-cd embuild
-source ~/Git/emsdk/emsdk_env.sh
-emcmake cmake -DCMAKE_BUILD_TYPE=Release ..
-emmake make
+#!/bin/sh
+set -e
+
+EMSDK_PATH="$HOME/Git/emsdk"
+BUILDDIR=embuild
+
+mkdir -p "$BUILDDIR"
+cp resources/* "$BUILDDIR/"
+for i in palette.lmp colormap.lmp maps/e1m1.bsp; do cp "$i" "$BUILDDIR/"; done
+. "$EMSDK_PATH/emsdk_env.sh"
+emcmake cmake -B "$BUILDDIR" -DCMAKE_BUILD_TYPE=Release
+emmake make -C "$BUILDDIR"
